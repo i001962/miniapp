fb3cdde301bc23f152eace3cfe7073050ee73579
mejango <mejango@protonmail.com>
2023-05-22 18:32:25 -0400

    merged

--- assets/scripts/tx.js
+++ assets/scripts/tx.js
@@ -1,6 +1,15 @@
+const getProvider = () => {
+  return new ethers.BrowserProvider(window.ethereum);
+}
+
 const getSigner = async () => {
-  const provider = new ethers.BrowserProvider(window.ethereum);
+  const provider = getProvider(); 
   return await provider.getSigner();
+}
+
+const view = async (contractAddress, contractAbi, fn, params) => {
+    const contract = new ethers.Contract(contractAddress, contractAbi, getProvider());
+    return await contract[fn](...params);
 }
 
 const sign = async (contractAddress, contractAbi, fn, params) => {
---
+++ assets/scripts/txs/croptop_publisher.js
@@ -0,0 +1,412 @@
+const croptopContract = async () => {
+    return "0x7c129e346a52bb7d366253d983b0992119c9b39b";
+}
+const contractABI = [{
+  "inputs": [
+    {
+      "internalType": "contract IJBController3_1",
+      "name": "_controller",
+      "type": "address"
+    },
+    {
+      "internalType": "uint256",
+      "name": "_feeProjectId",
+      "type": "uint256"
+    }
+  ], "stateMutability": "nonpayable",
+  "type": "constructor"
+},
+{
+  "inputs": [],
+  "name": "INCOMPATIBLE_DATA_SOURCE",
+  "type": "error"
+},
+{
+  "inputs": [],
+  "name": "INSUFFICIENT_AMOUNT",
+  "type": "error"
+},
+{
+  "inputs": [],
+  "name": "INSUFFICIENT_QUANTITY",
+  "type": "error"
+},
+{
+  "inputs": [],
+  "name": "INVALID_FEE_PERCENT",
+  "type": "error"
+},
+{
+  "inputs": [],
+  "name": "INVALID_MINIMUM_QUANTITY",
+  "type": "error"
+},
+{
+  "inputs": [],
+  "name": "UNAUTHORIZED",
+  "type": "error"
+},
+{
+  "inputs": [],
+  "name": "UNAUTHORIZED_CATEGORY",
+  "type": "error"
+},
+{
+  "anonymous": false,
+  "inputs": [
+    {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "projectId",
+      "type": "uint256"
+    },
+    {
+      "components": [
+        {
+          "internalType": "bytes32",
+          "name": "encodedIPFSUri",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint32",
+          "name": "quantity",
+          "type": "uint32"
+        },
+        {
+          "internalType": "uint88",
+          "name": "price",
+          "type": "uint88"
+        },
+        {
+          "internalType": "uint16",
+          "name": "category",
+          "type": "uint16"
+        }
+      ],
+      "indexed": false,
+      "internalType": "struct Post[]",
+      "name": "posts",
+      "type": "tuple[]"
+    },
+    {
+      "indexed": false,
+      "internalType": "address",
+      "name": "nftBeneficiary",
+      "type": "address"
+    },
+    {
+      "indexed": false,
+      "internalType": "address",
+      "name": "feeBeneficiary",
+      "type": "address"
+    },
+    {
+      "indexed": false,
+      "internalType": "uint256",
+      "name": "fee",
+      "type": "uint256"
+    },
+    {
+      "indexed": false,
+      "internalType": "address",
+      "name": "caller",
+      "type": "address"
+    }
+  ],
+  "name": "Collected",
+  "type": "event"
+},
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_projectId",
+      "type": "uint256"
+    },
+    {
+      "internalType": "address",
+      "name": "_nft",
+      "type": "address"
+    },
+    {
+      "internalType": "uint256",
+      "name": "_category",
+      "type": "uint256"
+    }
+  ],
+  "name": "allowanceFor",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "minimumPrice",
+      "type": "uint256"
+    },
+    {
+      "internalType": "uint256",
+      "name": "minimumQuantity",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_projectId",
+      "type": "uint256"
+    },
+    {
+      "components": [
+        {
+          "internalType": "bytes32",
+          "name": "encodedIPFSUri",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint32",
+          "name": "quantity",
+          "type": "uint32"
+        },
+        {
+          "internalType": "uint88",
+          "name": "price",
+          "type": "uint88"
+        },
+        {
+          "internalType": "uint16",
+          "name": "category",
+          "type": "uint16"
+        }
+      ],
+      "internalType": "struct Post[]",
+      "name": "_posts",
+      "type": "tuple[]"
+    },
+    {
+      "internalType": "address",
+      "name": "_nftBeneficiary",
+      "type": "address"
+    },
+    {
+      "internalType": "address",
+      "name": "_feeBeneficiary",
+      "type": "address"
+    }
+  ],
+  "name": "collect",
+  "outputs": [],
+  "stateMutability": "payable",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_projectId",
+      "type": "uint256"
+    },
+    {
+      "components": [
+        {
+          "internalType": "address",
+          "name": "nft",
+          "type": "address"
+        },
+        {
+          "internalType": "uint256",
+          "name": "category",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "minimumPrice",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "minimumQuantity",
+          "type": "uint256"
+        }
+      ],
+      "internalType": "struct AllowedPost[]",
+      "name": "_allowedPosts",
+      "type": "tuple[]"
+    }
+  ],
+  "name": "configure",
+  "outputs": [],
+  "stateMutability": "nonpayable",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "controller",
+  "outputs": [
+    {
+      "internalType": "contract IJBController3_1",
+      "name": "",
+      "type": "address"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "feeDivisor",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [],
+  "name": "feeProjectId",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_projectId",
+      "type": "uint256"
+    },
+    {
+      "internalType": "bytes32",
+      "name": "_encodedIPFSUri",
+      "type": "bytes32"
+    }
+  ],
+  "name": "tierIdForEncodedIPFSUriOf",
+  "outputs": [
+    {
+      "internalType": "uint256",
+      "name": "",
+      "type": "uint256"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+},
+{
+  "inputs": [
+    {
+      "internalType": "uint256",
+      "name": "_projectId",
+      "type": "uint256"
+    },
+    {
+      "internalType": "address",
+      "name": "_nft",
+      "type": "address"
+    },
+    {
+      "internalType": "bytes32[]",
+      "name": "_encodedIPFSUris",
+      "type": "bytes32[]"
+    }
+  ],
+  "name": "tiersFor",
+  "outputs": [
+    {
+      "components": [
+        {
+          "internalType": "uint256",
+          "name": "id",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "price",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "remainingQuantity",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "initialQuantity",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "votingUnits",
+          "type": "uint256"
+        },
+        {
+          "internalType": "uint256",
+          "name": "reservedRate",
+          "type": "uint256"
+        },
+        {
+          "internalType": "address",
+          "name": "reservedTokenBeneficiary",
+          "type": "address"
+        },
+        {
+          "internalType": "bytes32",
+          "name": "encodedIPFSUri",
+          "type": "bytes32"
+        },
+        {
+          "internalType": "uint256",
+          "name": "category",
+          "type": "uint256"
+        },
+        {
+          "internalType": "bool",
+          "name": "allowManualMint",
+          "type": "bool"
+        },
+        {
+          "internalType": "bool",
+          "name": "transfersPausable",
+          "type": "bool"
+        },
+        {
+          "internalType": "string",
+          "name": "resolvedUri",
+          "type": "string"
+        }
+      ],
+      "internalType": "struct JB721Tier[]",
+      "name": "tiers",
+      "type": "tuple[]"
+    }
+  ],
+  "stateMutability": "view",
+  "type": "function"
+}];
+
+const tx_view_allowance = async (projectId, category) => {
+  return await view(await croptopContract(), contractABI, "allowanceFor", [projectId, "0x0000000000000000000000000000000000000000", category]);
+}
+
+const tx_collect = async (projectId, category, quantity, price, encodedIPFSUri, beneficiary, cpnBeneficiary) => {
+  const bigIntPrice = BigInt(price);
+  const post = {quantity, price: bigIntPrice, category, encodedIPFSUri};
+  const divisor = BigInt("20");
+  if (!beneficiary) beneficiary = (await getSigner()).address;
+  if (!cpnBeneficiary) cpnBeneficiary = beneficiary; 
+  console.log({ projectId, posts: [post], beneficiary, cpnBeneficiary, value: bigIntPrice + (projectId == 748 ? BigInt(0) : (bigIntPrice / divisor)) });
+  return await sign(await croptopContract(), contractABI, "collect", [projectId, [post], beneficiary, cpnBeneficiary, {
+      value: bigIntPrice + (projectId == 748 ? BigInt(0) : (bigIntPrice / divisor)) 
+  }]);
+}
